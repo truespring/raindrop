e1906c1ed3d134c6c5e88b83a3b7794ad3e4a59a
truespring <truespring1@gmail.com>
2023-09-17 21:06:31 +0900

    1주 차 개발 완료

--- Assets/script/rain.cs
+++ Assets/script/rain.cs
@@ -4,10 +4,45 @@ using UnityEngine;
 
 public class rain : MonoBehaviour
 {
+    int type;
+    float size;
+    int score;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        float x = Random.Range(-2.7f, 2.7f);
+        float y = Random.Range(3.0f, 5.0f);
+        transform.position = new Vector3(x, y, 0);
+
+        type = Random.Range(1, 5);
+
+        if (type == 1)
+        {
+            size = 1.2f;
+            score = 1;
+            GetComponent<SpriteRenderer>().color = new Color(100 / 255f, 100 / 255f, 255 / 255f, 255 / 255f);
+        }
+        else if (type == 2)
+        {
+            size = 1.0f;
+            score = 2;
+            GetComponent<SpriteRenderer>().color = new Color(130 / 255f, 130 / 255f, 255 / 255f, 255 / 255f);
+
+        }
+        else if (type == 3)
+        {
+            size = 0.8f;
+            score = 3;
+            GetComponent<SpriteRenderer>().color = new Color(150 / 255f, 150 / 255f, 255 / 255f, 255 / 255f);
+        }
+        else if (type == 4)
+        {
+            size = 0.8f;
+            score = -5;
+            GetComponent<SpriteRenderer>().color = new Color(255 / 255f, 100 / 255f, 100 / 255f, 255 / 255f);
+        }
+        transform.localScale = new Vector3(size, size, 0);
     }
 
     // Update is called once per frame
@@ -20,7 +55,13 @@ public class rain : MonoBehaviour
     {
         if (coll.gameObject.tag == "ground")
         {
-            Debug.Log("땅을 만남");
+            Destroy(gameObject);
+        }
+
+        if (coll.gameObject.tag == "rtan")
+        {
+            Destroy(gameObject);
+            GameManager.I.addScore(score);
         }
     }
 }
